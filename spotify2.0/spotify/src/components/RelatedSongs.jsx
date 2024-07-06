ea34711eb38d07d554039e0a66b82d772c0afc27
Anjalisen66 <anjalisen8203@gmail.com>
2024-07-06 23:16:24 +0530

    first commit

---
+++ spotify2.0/spotify/src/components/RelatedSongs.jsx
@@ -0,0 +1,32 @@
+import SongBar from "./SongBar";
+
+const RelatedSongs = ({
+  data,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+  artistId,
+}) => {
+  return (
+    <div className="flex flex-col">
+      <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+      <div className="mt-6 w-full flex flex-col ">
+        {data?.filter((el) => {
+            return el.images !== undefined;
+          }).map((song, i) => (
+          <SongBar
+            key={`${song.key}-${artistId}`}
+            song={song}
+            i={i}
+            artistId={artistId}
+            handlePauseClick={handlePauseClick}
+            handlePlayClick={handlePlayClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RelatedSongs;
